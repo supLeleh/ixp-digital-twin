6b26bf697f63c7236ddf98cb9b192c4bda203393
supLeleh <149876400+supLeleh@users.noreply.github.com>
2025-11-14 15:17:32 +0100

    inizio progetto

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FileManager from './components/FileManager';
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1>Semplice Gestore File</h1>
+      <FileManager />
+    </div>
+  );
+}
+
+export default App;
+
